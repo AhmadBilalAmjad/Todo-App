8b658ae0da30dec55ff965598a32613674035134
Ahmad Bilal Amjad <ahmad.bilal.amjad@outlook.com>
2019-03-16 13:57:50 +0500

    Deployment Setup For Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
-const url = "mongodb://localhost:27017/TodoApp";
 
 mongoose.Promise = global.Promise;
-mongoose.connect(url);
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp"
+);
 
 module.exports = { mongoose };
